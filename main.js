8bca5a6889f21f19e35320e78c62a871e702a098
Bima <bimasakti414@gmail.com>
2019-01-04 16:32:45 +0700

    after a while updated again

---
+++ main.js
@@ -0,0 +1,21 @@
+
+  document.addEventListener('DOMContentLoaded', function () {
+    var elems = document.querySelectorAll('.slider');
+    var instances = M.Slider.init(elems);
+  });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.parallax');
+    var instances = M.Parallax.init(elems);
+  });
+
+// Add active class to the current button (highlight it)
+var header = document.getElementById("mySide");
+var btns = header.getElementsByClassName("b");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+  var current = document.getElementsByClassName("active");
+  current[0].className = current[0].className.replace(" active", "");
+  this.className += " active";
+  });
+}
